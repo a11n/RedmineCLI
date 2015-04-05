6ac6eedac19fe819aa08beb796b1bb31be027951
a11n <andre.diermann@gmail.com>
2015-04-05 20:10:13 +0200

    Refactored CLI.

---
+++ module/actions.js
@@ -0,0 +1,29 @@
+var redmine = require('../module/redmine.js');
+var printer = require('../module/printer.js');
+
+exports.handleConnect = function(url, apiKey){
+  try{
+    redmine.connect(url, apiKey);
+  } catch(err){console.error(err)}
+}
+
+exports.handleProjects = function(){
+  try{
+    var projects = redmine.getProjects();
+    printer.printProjects(projects.projects);
+  } catch(err){console.error(err)}
+}
+
+exports.handleIssues = function(options){
+  try{
+    var issues = redmine.getIssues();
+    printer.printIssues(issues.issues);
+  } catch(err){console.error(err)}
+}
+
+exports.handleIssue = function(id){
+  try{
+    var issue = redmine.getIssue(id);
+    printer.printIssue(issue.issue);
+  } catch(err){console.error(err)}
+}
