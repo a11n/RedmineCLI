213d4da675867ae31c039f31135a7033be7983af
a11n <andre.diermann@gmail.com>
2015-04-06 11:04:03 +0200

    Improved templating.

--- module/printer.js
+++ module/printer.js
@@ -1,26 +1,8 @@
-var Table = require('cli-table');
 var tmpl = require('./template-engine.js');
 
 exports.printIssues = function(issues){
-  var table = new Table({
-      head: ['ID', 'Tracker', 'Status', 'Priority', 'Assignee', 'Updated']
-  });
-  for(var i=0; i<issues.length;i++){
-    var issue = issues[i];
-    var id = issue.id;
-    var tracker = issue.tracker.name;
-    var status = issue.status.name;
-    var priority = issue.priority.name;
-    var assignee;
-    if(!!issue.assigned_to)
-      assignee = issue.assigned_to.name;
-    else
-      assignee = '(not assigned)';
-
-    table.push([id, tracker, status, priority, assignee, issue.updated_on]);
-  }
-
-  console.log(table.toString());
+  var out = tmpl.renderFile('template/issues.tmpl', {'issues': issues});
+  console.log(out);
 }
 
 exports.printIssue = function(issue){
@@ -29,16 +11,6 @@ exports.printIssue = function(issue){
 }
 
 exports.printProjects = function(projects){
-  var table = new Table({
-      head: ['Name', 'Key']
-  });
-  for(var i=0; i<projects.length;i++){
-    var project = projects[i];
-    var name = project.name;
-    var identifier = project.identifier;
-
-    table.push([name, identifier]);
-  }
-
-  console.log(table.toString());
+  var out = tmpl.renderFile('template/projects.tmpl', {'projects': projects});
+  console.log(out);
 }
--- module/template-engine.js
+++ module/template-engine.js
@@ -1,8 +1,21 @@
 var swig = require('swig');
 var moment = require('moment');
+var Table = require('cli-table');
 
 swig.setFilter('ago', function (input) {
   return moment(input).fromNow();
+});
+
+swig.setFilter('table', function (input, head) {
+  var table = head ? new Table({'head': head}) : new Table();
+
+  var rows = input.split('\n'); rows.pop();
+  for(var i=0; i<rows.length; i++){
+    var columns = rows[i].split('|');
+    table.push(columns);
+  }
+
+  return table.toString();
 });
 
 exports.renderFile = function(file, data){
