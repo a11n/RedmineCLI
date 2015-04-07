cd313a8f815ddbe509674e330d3da6788c316534
a11n <andre.diermann@gmail.com>
2015-04-07 10:06:13 +0200

    Improved issues presentation. Removed table lines to display more actual data on screen.

--- module/template-engine.js
+++ module/template-engine.js
@@ -12,7 +12,12 @@ swig.setFilter('heading', function (input) {
 });
 
 swig.setFilter('table', function (input, head) {
-  var table = head ? new Table({'head': head}) : new Table();
+  var style =  { 'padding-left': 0, 'padding-right': 1, 'head': ['bold'] };
+  var chars = { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+         , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+         , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+         , 'right': '' , 'right-mid': '' , 'middle': ' ' };
+  var table = new Table({'chars': chars, 'style': style, 'head': head || []});
 
   var rows = input.split('\n'); rows.pop();
   for(var i=0; i<rows.length; i++){
